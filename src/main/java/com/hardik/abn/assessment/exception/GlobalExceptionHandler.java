b97cb2acc0d3eff7502c1e68bbddf17706872d3b
Rajani <hardik.rajani@capgemini.com>
2020-10-26 11:52:39 +0100

    Sonar improvement

--- src/main/java/com/hardik/abn/assessment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hardik/abn/assessment/exception/GlobalExceptionHandler.java
@@ -12,19 +12,19 @@ import org.springframework.web.context.request.WebRequest;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RecipeNotFoundException.class)
-    public ResponseEntity<?> resourceNotFoundException(RecipeNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ErrorDetails> resourceNotFoundException(RecipeNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RecipeAttributeNotFoundException.class)
-    public ResponseEntity<?> recipeAttributeNotFoundException(RecipeAttributeNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ErrorDetails> recipeAttributeNotFoundException(RecipeAttributeNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
  
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+    public ResponseEntity<ErrorDetails> globleExcpetionHandler(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
